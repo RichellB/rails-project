93a0371c164448566f5d6c34fae01b586ed2ec6c
Richell Bonfield <richell.bonfield12@gmail.com>
2019-12-09 03:54:37 +0000

    Added validations to model files

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,11 @@
 class Artist < ApplicationRecord
   has_many :songs
   has_many :genres, through: :songs
+  has_secure_password 
+  
+  validates_presence_of :name, :email_address, :username
+  validates :username, uniqueness: true 
+  validates :email_address, uniqueness: true 
+  validates :password, length: { in: 8..16 }
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,7 @@
 class Song < ApplicationRecord
   belongs_to :artist
   belongs_to :genre
+  
+  validates :title, presence: true
+  validates :album_name, presence: true
 end 
