25ac3882701f57c4b8102e8ec04d7d55c51afe6c
Richell Bonfield <richell.bonfield12@gmail.com>
2019-12-26 02:40:36 +0000

    Modified tables and layouts with devise

--- db/migrate/20191226022645_add_username_to_users.rb
+++ db/migrate/20191226022645_add_username_to_users.rb
@@ -1,4 +1,4 @@
-class AddUsernameToUsers < ActiveRecord::Migration[6.0]
+class AddUsernameToUsers < ActiveRecord::Migration[4.2]
   def change
     add_column :users, :username, :string
     add_column :users, :name, :string
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20191226022645) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string   "name"
+    t.text     "email"
+    t.string   "username"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string   "genre_name"
+    t.string   "artist_id"
+    t.string   "song_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string   "title"
+    t.string   "album_name"
+    t.string   "artist_name"
+    t.string   "genre_name"
+    t.string   "release_year"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "artist_id"
+    t.index ["artist_id"], name: "index_songs_on_artist_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string   "username"
+    t.string   "name"
+    t.string   "company"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
